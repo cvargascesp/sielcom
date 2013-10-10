60b382c132d45f7274542c1e2e9fcacbcbcb5372
machine666 <cv302219@gmail.com>
2013-10-10 11:39:18 -0300

    db_setting
    cambiaron las conexiones a la base de datos

--- WindowsApplication1/Conexion.vb
+++ WindowsApplication1/Conexion.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Conexion
-    Public Shared strConn As String = "server=isystem.cl;database=sistema_sielcom;user=admin_sistema;password=PassSielcom123;port=3306;Connection Lifetime=86400"
+    Public Shared strConn As String = "server=sielcom.db.11889148.hostedresource.com;database=sielcom;user=sielcom;password=AAbbcc123#;port=3306;Connection Lifetime=86400"
     'Public Shared strConn As String = "server=localhost;database=sistema_jerplaz;user=root;password=gprl128x;port=3306;Connection Lifetime=86400"
     Public Shared conn As MySql.Data.MySqlClient.MySqlConnection
     Public Shared strCriticos As String = "select codigo as Código,nombre as Nombre,stock as Stock,critico as Critico, porvender 'Por vender',porllegar 'Por llegar', cast(fechapedido as date) as 'Fecha Pedido', descripcion as Descripción from producto where critico >= stock + porllegar - porvender"
