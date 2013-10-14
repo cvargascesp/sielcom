e810e10aa836e9bfc0e3f9a7ba31bd6bec769188
machine666 <cv302219@gmail.com>
2013-10-14 15:51:13 -0300

    agregar orden de compra
    se agrega la funcionabilidad de la orden de compra

--- WindowsApplication1/i_libro_pedido.Designer.vb
+++ WindowsApplication1/i_libro_pedido.Designer.vb
@@ -22,6 +22,7 @@ Partial Class i_libro_pedido
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(i_libro_pedido))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.fecha_ingreso = New System.Windows.Forms.DateTimePicker()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -29,17 +30,17 @@ Partial Class i_libro_pedido
         Me.Label4 = New System.Windows.Forms.Label()
         Me.fecha_tope = New System.Windows.Forms.DateTimePicker()
         Me.label5 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtmateriaprima = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.txtcantmp = New System.Windows.Forms.NumericUpDown()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txt_unidadmedida = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label8 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.txtoc_mp = New System.Windows.Forms.TextBox()
+        CType(Me.txtcantmp, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -101,12 +102,12 @@ Partial Class i_libro_pedido
         Me.label5.TabIndex = 8
         Me.label5.Text = "Producto:"
         '
-        'TextBox1
+        'txtmateriaprima
         '
-        Me.TextBox1.Location = New System.Drawing.Point(494, 23)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(240, 20)
-        Me.TextBox1.TabIndex = 9
+        Me.txtmateriaprima.Location = New System.Drawing.Point(494, 23)
+        Me.txtmateriaprima.Name = "txtmateriaprima"
+        Me.txtmateriaprima.Size = New System.Drawing.Size(240, 20)
+        Me.txtmateriaprima.TabIndex = 9
         '
         'Label6
         '
@@ -117,12 +118,12 @@ Partial Class i_libro_pedido
         Me.Label6.TabIndex = 10
         Me.Label6.Text = "Cantidad:"
         '
-        'NumericUpDown1
+        'txtcantmp
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(494, 87)
-        Me.NumericUpDown1.Name = "NumericUpDown1"
-        Me.NumericUpDown1.Size = New System.Drawing.Size(240, 20)
-        Me.NumericUpDown1.TabIndex = 11
+        Me.txtcantmp.Location = New System.Drawing.Point(494, 87)
+        Me.txtcantmp.Name = "txtcantmp"
+        Me.txtcantmp.Size = New System.Drawing.Size(240, 20)
+        Me.txtcantmp.TabIndex = 11
         '
         'Label7
         '
@@ -133,40 +134,49 @@ Partial Class i_libro_pedido
         Me.Label7.TabIndex = 12
         Me.Label7.Text = "Unidad medida:"
         '
-        'TextBox2
+        'txt_unidadmedida
         '
-        Me.TextBox2.Location = New System.Drawing.Point(494, 57)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(240, 20)
-        Me.TextBox2.TabIndex = 13
+        Me.txt_unidadmedida.Location = New System.Drawing.Point(494, 57)
+        Me.txt_unidadmedida.Name = "txt_unidadmedida"
+        Me.txt_unidadmedida.ReadOnly = True
+        Me.txt_unidadmedida.Size = New System.Drawing.Size(240, 20)
+        Me.txt_unidadmedida.TabIndex = 13
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(18, 167)
+        Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(15, 161)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(183, 34)
+        Me.Button1.Size = New System.Drawing.Size(106, 52)
         Me.Button1.TabIndex = 14
         Me.Button1.Text = "Agregar"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button1.UseVisualStyleBackColor = True
         '
         'DataGridView1
         '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
         Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLight
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.GridColor = System.Drawing.SystemColors.ControlLight
-        Me.DataGridView1.Location = New System.Drawing.Point(12, 219)
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 219)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(716, 150)
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(750, 150)
         Me.DataGridView1.TabIndex = 15
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(240, 411)
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(282, 417)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(255, 49)
+        Me.Button2.Size = New System.Drawing.Size(165, 58)
         Me.Button2.TabIndex = 16
         Me.Button2.Text = "Generar pedido"
+        Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Label8
@@ -178,28 +188,28 @@ Partial Class i_libro_pedido
         Me.Label8.TabIndex = 17
         Me.Label8.Text = "Orden de compra:"
         '
-        'TextBox3
+        'txtoc_mp
         '
-        Me.TextBox3.Location = New System.Drawing.Point(122, 57)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(200, 20)
-        Me.TextBox3.TabIndex = 18
+        Me.txtoc_mp.Location = New System.Drawing.Point(122, 57)
+        Me.txtoc_mp.Name = "txtoc_mp"
+        Me.txtoc_mp.Size = New System.Drawing.Size(200, 20)
+        Me.txtoc_mp.TabIndex = 18
         '
         'i_libro_pedido
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(777, 498)
-        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.txtoc_mp)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.txt_unidadmedida)
         Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.txtcantmp)
         Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtmateriaprima)
         Me.Controls.Add(Me.label5)
         Me.Controls.Add(Me.fecha_tope)
         Me.Controls.Add(Me.Label4)
@@ -209,7 +219,7 @@ Partial Class i_libro_pedido
         Me.Controls.Add(Me.Label1)
         Me.Name = "i_libro_pedido"
         Me.Text = "i_libro_pedido"
-        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtcantmp, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -222,14 +232,14 @@ Partial Class i_libro_pedido
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents fecha_tope As System.Windows.Forms.DateTimePicker
     Friend WithEvents label5 As System.Windows.Forms.Label
-    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtmateriaprima As System.Windows.Forms.TextBox
     Friend WithEvents Label6 As System.Windows.Forms.Label
-    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents txtcantmp As System.Windows.Forms.NumericUpDown
     Friend WithEvents Label7 As System.Windows.Forms.Label
-    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents txt_unidadmedida As System.Windows.Forms.TextBox
     Friend WithEvents Button1 As System.Windows.Forms.Button
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
     Friend WithEvents Button2 As System.Windows.Forms.Button
     Friend WithEvents Label8 As System.Windows.Forms.Label
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtoc_mp As System.Windows.Forms.TextBox
 End Class
