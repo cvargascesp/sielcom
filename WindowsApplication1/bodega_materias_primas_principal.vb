85e9d2cc701eb66451ff7b65eedc135da6b292b4
machine666 <cv302219@gmail.com>
2013-10-29 16:28:15 -0300

    matetrias primas
    fix boton volver
    carga datagrid stock critico

--- WindowsApplication1/bodega_materias_primas_principal.vb
+++ WindowsApplication1/bodega_materias_primas_principal.vb
@@ -1,4 +1,5 @@
-﻿Public Class bodega_materias_primas_principal
+﻿Imports MySql.Data.MySqlClient
+Public Class bodega_materias_primas_principal
 
     Private Sub Button6_Click(sender As Object, e As EventArgs)
         Dim result As DialogResult
@@ -21,9 +22,9 @@
 
     End Sub
 
-    
 
- 
+
+
 
     Private Sub PcesoDeFabricacionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PcesoDeFabricacionToolStripMenuItem.Click
 
@@ -33,7 +34,7 @@
         solicitud_productos_fabricacion.Show()
 
     End Sub
-    
+
     Private Sub ConsultaDeProcesoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ConsultaDeProcesoToolStripMenuItem.Click
         proceso_de_fabricacion.Show()
     End Sub
@@ -54,7 +55,7 @@
         catalogo_mp.Show()
     End Sub
 
-    
+
     Private Sub KardexToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KardexToolStripMenuItem.Click
         kardex_mp.Show()
     End Sub
@@ -71,7 +72,23 @@
         libro_ingreso_mercaderia.Show()
     End Sub
 
+    
+    Sub llenar_datagrid_stock_critico()
+        Conexion.open()
+        Dim query As String = "SELECT 	materia_prima.codigo_mp 'Codigo materia Prima', materia_prima.nombre_mp 'nombre', materia_prima.unidad_medida_mp'Unidad de medida', familias.nom_familia'Familia', COALESCE(materia_prima_existencias.stock_mp,0)'stock Actual', materia_prima.stock_critico_mp'Stock Critico'FROM materia_prima LEFT JOIN materia_prima_existencias ON materia_prima_existencias.codigo_mp=materia_prima.codigo_mp INNER JOIN familias ON familias.idfamilia=materia_prima.idfamilia  WHERE COALESCE(materia_prima_existencias.stock_mp,0)<= materia_prima.stock_critico_mp"
+        Dim Adpt As New MySqlDataAdapter(query, Conexion.conn)
+        Dim ds As New DataSet()
+        Adpt.Fill(ds, "Emp")
+        DataGridView1.DataSource = ds.Tables(0)
+        Conexion.close()
+    End Sub
+
+    Private Sub bodega_materias_primas_principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenar_datagrid_stock_critico()
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
         inicio.Show()
     End Sub
 End Class
